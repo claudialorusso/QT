a3b48525f739cc2cb40f8f2f88f1cc6bd022c72d
Angela Dileo <angidili96@gmail.com>
2019-06-28 10:53:02 +0200

    Inizio della classe ServerOneClient

--- QTServer/src/server/ServerOneClient.java
+++ QTServer/src/server/ServerOneClient.java
@@ -1,5 +1,28 @@
 package server;
 
-public class ServerOneClient {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import mining.QTMiner;
+
+public class ServerOneClient extends Thread {
+	private Socket socket;
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
+	private QTMiner kmeans;
+	
+	public ServerOneClient(Socket s) throws IOException {
+		this.socket = s;
+		in = new ObjectInputStream(s.getInputStream());
+		out = new ObjectOutputStream(s.getOutputStream());
+		
+		start(); //avvio del thread
+	}
+	
+	public void run(){
+		
+	}
 
 }

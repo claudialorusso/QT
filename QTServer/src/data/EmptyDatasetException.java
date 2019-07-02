b31019c92626cc048e4181e15d0778658fdf523b
Claudia Lorusso <lorusso.claudia@outlook.it>
2019-07-02 18:05:15 +0200

    modificata JavaDoc in EmptyDatasetException

--- QTServer/src/data/EmptyDatasetException.java
+++ QTServer/src/data/EmptyDatasetException.java
@@ -1,16 +1,18 @@
 package data;
-
 /**
  * Modella un'eccezione controllata da considerare qualora
- * il dataset sia vuoto. 
+ * il dataset sia vuoto.
  * @author Lorusso Claudia, Dileo Angela
  */
 public class EmptyDatasetException extends Exception{
+	/**
+	 * ID di serializzazione
+	 */
 	private static final long serialVersionUID = 1L;
 	/**
 	 * Costruttore della classe EmptyDatasetException.
 	 * Stampa a video un messaggio di errore nel caso 
-	 * in cui il dataset è vuoto.
+	 * in cui il dataset e' vuoto.
 	 */
 	public EmptyDatasetException(){
 		super("ERROR:The Data set is empty!");

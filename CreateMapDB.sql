a7b82b226d67d8b2d0b166b702a20ebe8b4f5caa
Claudia Lorusso <lorusso.claudia@outlook.it>
2019-11-21 10:32:01 +0100

    aggiunti file pdf definitivi

--- CreateMapDB.sql
+++ CreateMapDB.sql
@@ -29,43 +29,6 @@ insert into MapDB.playtennis values('overcast',12.5,'high','strong','yes');
 insert into MapDB.playtennis values('overcast',29.21,'normal','weak','yes');
 insert into MapDB.playtennis values('rain',12.5,'high','strong','no');
 
-CREATE TABLE MapDB. example1(
-	id INT PRIMARY KEY,
-	x DOUBLE,
-	y DOUBLE
-);
-
-insert into MapDB.example1 values
-	(1,1,1),
-	(2,0,1),
-	(3,1,0),
-	(4,11,12),
-	(5,11,13),
-	(6,13,13),
-	(7,12,8.5),
-	(8,13,8),
-	(9,13,9),
-	(10,13,7),
-	(11,11,7),
-	(12,8,2), 
-	(13,9,2),
-	(14,10,1),
-	(15,7,13),
-	(16,5,9),
-	(17,16,16),
-	(18,11.5,8),
-	(20,13,10),
-	(21,12,13),
-	(31,14,12.5),
-	(22,14.5,11.5),
-	(23,15,10.5),
-	(24,15,9.5),
-	(25,12,9.5),
-	(26,10.5,11),
-	(27,10,10.5),
-	(28,9,3),
-	(29,9,4),
-	(30,9,5);
 CREATE TABLE MapDB. emptyt(
 	id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(30)
